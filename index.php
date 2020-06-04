a3db52833a602dc4f17ab442e73b568cd8ea649c
Gerardo Pagliarulo <gerardopagliarulo3@gmail.com>
2020-06-04 19:16:16 +0200

    PHP - OOP - First

--- index.php
+++ index.php
@@ -33,31 +33,31 @@ include_once __DIR__ . '/classes/class-movie.php';
                 <td><?php echo $movie1->ageMovie(2020); ?></td>
             </tr>
             <tr class="line-h-35">
-                <?php $movie1 = new Movie('Five Easy Pieces', 'Bob Rafelson', '1970', 'Drama', 96); ?>
-                <td><?php echo $movie1->title; ?></td>
-                <td><?php echo $movie1->director; ?></td>
-                <td><?php echo $movie1->year; ?></td>
-                <td><?php echo $movie1->genre; ?></td>
-                <td><?php echo $movie1->running_time;?>min</td>
-                <td><?php echo $movie1->ageMovie(2020); ?></td>
+                <?php $movie2 = new Movie('Five Easy Pieces', 'Bob Rafelson', '1970', 'Drama', 96); ?>
+                <td><?php echo $movie2->title; ?></td>
+                <td><?php echo $movie2->director; ?></td>
+                <td><?php echo $movie2->year; ?></td>
+                <td><?php echo $movie2->genre; ?></td>
+                <td><?php echo $movie2->running_time;?>min</td>
+                <td><?php echo $movie2->ageMovie(2020); ?></td>
             </tr>
             <tr class="line-h-35">
-                <?php $movie1 = new Movie('Dillinger è morto', 'Marco Ferreri', '1969', 'Drama', 95); ?>
-                <td><?php echo $movie1->title; ?></td>
-                <td><?php echo $movie1->director; ?></td>
-                <td><?php echo $movie1->year; ?></td>
-                <td><?php echo $movie1->genre; ?></td>
-                <td><?php echo $movie1->running_time; ?>min</td>
-                <td><?php echo $movie1->ageMovie(2020); ?></td>
+                <?php $movie3 = new Movie('Dillinger è morto', 'Marco Ferreri', '1969', 'Drama', 95); ?>
+                <td><?php echo $movie3->title; ?></td>
+                <td><?php echo $movie3->director; ?></td>
+                <td><?php echo $movie3->year; ?></td>
+                <td><?php echo $movie3->genre; ?></td>
+                <td><?php echo $movie3->running_time; ?>min</td>
+                <td><?php echo $movie3->ageMovie(2020); ?></td>
             </tr>
             <tr class="line-h-35">
-                <?php $movie1 = new Movie('La grande guerra', 'Mario Monicelli', '1959', 'Drama', 135); ?>
-                <td><?php echo $movie1->title; ?></td>
-                <td><?php echo $movie1->director; ?></td>
-                <td><?php echo $movie1->year; ?></td>
-                <td><?php echo $movie1->genre; ?></td>
-                <td><?php echo $movie1->running_time; ?>min</td>
-                <td><?php echo $movie1->ageMovie(2020); ?></td>
+                <?php $movie4 = new Movie('La grande guerra', 'Mario Monicelli', '1959', 'Drama', 135); ?>
+                <td><?php echo $movie4->title; ?></td>
+                <td><?php echo $movie4->director; ?></td>
+                <td><?php echo $movie4->year; ?></td>
+                <td><?php echo $movie4->genre; ?></td>
+                <td><?php echo $movie4->running_time; ?>min</td>
+                <td><?php echo $movie4->ageMovie(2020); ?></td>
             </tr>
         </table>
     </main>
